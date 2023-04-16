1eb62517d98bd63e6df2262f2e312d9e115f14aa
abhinavkala <61939672+abhinavkala@users.noreply.github.com>
2023-04-17 02:55:42 +0530

    Update follow a a mentioned person.py

--- follow a a mentioned person.py
+++ follow a a mentioned person.py
@@ -1,4 +1,5 @@
 from InstagramAPI import InstagramAPI
+import random
 
 # set up the Instagram API client
 api = InstagramAPI(username, password)
@@ -9,18 +10,31 @@ def follow_user(username):
     api.searchUsername(username)
     user_id = api.LastJson['user']['pk']
     api.follow(user_id)
-    print(f"Successfully followed {username}!")
+    return f"Successfully followed {username} on Instagram!"
 
-# define a function to handle incoming chat messages
+# define a function to handle user input
 def handle_message(message):
+    # generate a random response
+    responses = ["I'm sorry, I don't understand.",
+                 "Sure, I can follow that person for you.",
+                 "Please provide a valid username."]
+    response = random.choice(responses)
+
+    # check if the message contains the word "follow"
     if 'follow' in message.lower():
         # extract the username from the message
         username = message.split('follow ')[-1]
-        follow_user(username)
-    else:
-        print("Sorry, I don't understand.")
+        response = follow_user(username)
 
-# simulate incoming chat messages
-messages = ['Can you follow @john.doe?', 'Follow @jane.smith please'] #add more of keywords
-for message in messages:
-    handle_message(message)
+    return response
+
+# simulate a conversation with the chatbot
+while True:
+    # get user input
+    message = input("You: ")
+
+    # handle user input
+    response = handle_message(message)
+
+    # print chatbot response
+    print("Chatbot:", response)
